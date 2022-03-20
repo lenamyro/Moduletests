be0d7f8619967350008b3ffc9ec33c4a57c3fd2f
omyrosnikova <lena.myroshnykova@gmail.com>
2022-03-20 21:07:31 +0100

    fixed comments

--- QuadraticHelper.cs
+++ QuadraticHelper.cs
@@ -13,19 +13,35 @@
 
         public static double[] Solve(double a, double b, double c)
         {
-            if (a == default)
+            
+            try {
+                ParametersValidation(a, b, c);
+                
+                double d = b * b - 4 * a * c;
+
+                var x1 = (-b + Math.Sqrt(d)) / 2 * a;
+                var x2 = (-b - Math.Sqrt(d)) / 2 * a;
+
+                return new double[] { x1, x2 };
+            } catch()
+            {
+            }
+
+        }
+
+        private static void ParametersValidation(double a, double b, double c)
+        {
+            if (a.EqualsExact(0))
                 throw new ArgumentException("Argument a can not be 0");
 
+            if (double.IsFinite(a) || double.IsNaN(a))
+                throw new ArgumentException("Argument a is incorrect");
 
-            double d = b * b - 4 * a * c;
+            if (double.IsFinite(b) || double.IsNaN(b))
+                throw new ArgumentException("Argument b is incorrect");
 
-            if (d.EqualsExact(0))
-                throw new ArgumentException("Diskriminant is 0");
-
-            var x1 = (-b + Math.Sqrt(d)) / 2 * a;
-            var x2 = (-b - Math.Sqrt(d)) / 2 * a;
-
-            return new double[] { x1, x2 };
+            if (double.IsFinite(c) || double.IsNaN(c))
+                throw new ArgumentException("Argument c is incorrect");
         }
     }
 }
